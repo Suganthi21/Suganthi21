a51b409a7804208f8f21d63e62c31ecd19ad2554
Suganthi21 <mathumithasuganthi@gmail.com>
2021-05-31 16:25:20 +0530

    b2 added code

--- src/org/testing/CourseInformation.java
+++ src/org/testing/CourseInformation.java
@@ -2,9 +2,10 @@ package org.testing;
 
 public class CourseInformation {
 
+	int a=100;
 	
 	private void javaCourseInfo() {
-
+		System.out.println("Java Information");
 		System.out.println("Java class started..");
 		
 	}
